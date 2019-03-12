2a7c3a96bb261f13bb8e57d2d4741002c192c116
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-03-12 14:04:52 +0200

    Update Basiccheck.sh

--- Basiccheck.sh
+++ Basiccheck.sh
@@ -3,20 +3,33 @@
 fileName=$1
 project=$2
 
+#A third and next variable if needed
 variables=${@:3}
 
+#Login to file
 cd $fileName
+
+# Throw the action
 make &> /dev/null
 
+#Tracking and testing
+comp= "PASS" 
+memory_leak= "PASS"
+thread_race= "PASS"
+Answer="$Comp$Memoryleake$Threadrace"
 
 if [[$? -gt 0]]; then
      echo "compilation  memory leaks  thread race"
+     comp= "fail"
+     memory_leak= "fail"
+     thread_race= "fail"
      echo " fail           fail           fail  "
      exit 7
  else 
+     
    comp=0
    
-   valgrind --leak-check=full --error-exitcode=1 
+   valgrind --leak-check=full --error-exitcode=1 -q ./$project $@ >/dev/null 2>&1 
    if [[ $? -gt 0 ]]; then
          Memory=1
          else
@@ -24,32 +37,38 @@ if [[$? -gt 0]]; then
     fi
     
     valgrind --tool=helgrind --error-exitcode=1 
-    if [[ $? -gt  ]]; then
+    if [[ $? -gt 0 ]]; then
            Thread=1
            else
            Thread=0
     fi
     
-    Answer="$comp$Memory$Thread"
+   
     if [[ $Answer -eq "000" ]]; then
+        
           echo "compilation  memory leaks  thread race"
           echo "   pass          pass           pass   "
           exit 0
     fi
     
+    
     if [[ $Answer -eq "001"]]; then
+          thread_race= "FAIL"
           echo "compilation  memory leaks  thread race"
           echo "   pass          pass           fail
           exit 1
     fi
     
     if [[ $Answer -eq "010"]]; then
+          memory_leak= "FAIL"
           echo "compilation  memory leaks  thread race"
           echo "   pass          fail      pass   "
           exit 2
     fi
     
     if [[ $Answer -eq "011" ]]; then
+           memory_leak= "FAIL"
+           thread_race= "FAIL"
           echo "compilation  memory leaks  thread race"
           echo "   pass          fail           fail  "
           exit 3

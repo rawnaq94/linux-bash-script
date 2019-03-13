d58f976e3fd6c142c6931f71a8b2cb285e6eb925
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-03-13 14:21:37 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -20,27 +20,30 @@ Answer="$Comp$MemoryLeake$ThreadRace"
 
 if [ $? -gt 0 ]; then
      echo "compilation  memory leaks  thread race"
-     compilation="FAIL"
-     memoryLeak="FAIL"
-     threadRace="FAIL"
+     $compilation="FAIL"
+     $memoryLeak="FAIL"
+     $threadRace="FAIL"
      echo " fail           fail           fail  "
      exit 7
  else 
      
    comp=0
    
-   valgrind --leak-check=full --error-exitcode=1 -q ./$project $@ >/dev/null 2>&1 
-   if [ $? -gt 0 ]; then
-         Memory=1
-         else
-         Memory=0
+   valgrind --leak-check=full --error-exitcode=1 ./$project
+            Memory=$?
+
+   if [ Memory -eq 0 ]; then
+     echo " Memory "
+     echo " PASS "
+     
     fi
     
-    valgrind --tool=helgrind --error-exitcode=1 ./$project $@ >/dev/null 2>&1 
-    if [ $? -gt 0 ]; then
-           Thread=1
-           else
-           Thread=0
+    valgrind --tool=helgrind --error-exitcode=1 ./$project
+           Thread=&?
+    if [ Thread -eq 0 ]; then
+             echo " Thread "
+             echo " PASS "
+           
     fi
     
    
@@ -53,24 +56,25 @@ if [ $? -gt 0 ]; then
     
     
     if [ $Answer -eq "001" ]; then
-          threadRace="FAIL"
+          $threadRace="FAIL"
           echo "compilation  memory leaks  thread race"
           echo "   pass          pass           fail  "
           exit 1
     fi
     
     if [ $Answer -eq "010" ]; then
-          memoryLeak="FAIL"
+          $memoryLeak="FAIL"
           echo "compilation  memory leaks  thread race"
           echo "   pass          fail          pass   "
           exit 2
     fi
     
     if [ $Answer -eq "011" ]; then
-           memoryLeak="FAIL"
-           threadRace="FAIL"
+           $memoryLeak="FAIL"
+           $threadRace="FAIL"
           echo "compilation  memory leaks  thread race"
           echo "   pass          fail         fail    "
           exit 3
     fi
     fi
+    exit 0
